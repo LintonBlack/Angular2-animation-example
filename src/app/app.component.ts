e070962b0b0fe1b8a22375a9abe3320d26b90e05
Phil <phil@metrodesk.com>
2017-01-04 13:58:58 +0000

    onclick trigger go

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,25 @@ import { Component, trigger, state, style } from '@angular/core';
   	trigger('signal', [
   		state('go', style({
   			'background-color':'green'
+  		})),
+  		state('stop', style({
+  			'background-color':'red'
   		}))
   	])
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  
+  signal;
+
+  constructor() {
+  	this.signal = 'stop'
+  }
+
+  onClick(step) {
+  	this.signal = step;
+  }
 }
