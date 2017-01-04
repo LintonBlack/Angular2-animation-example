02154d5e1d11643a102c4d0dc36c411d9fa23333
Phil <phil@metrodesk.com>
2017-01-04 15:10:38 +0000

    transition between any initial state aka void

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,16 @@ import { Component, trigger, state, style, transition, animate } from '@angular/
   selector: 'app-root',
   animations:[
   	trigger('signal', [
+  		state('void', style({
+  			'background-color':'orange'
+  		})),
   		state('go', style({
   			'background-color':'green'
   		})),
   		state('stop', style({
   			'background-color':'red'
   		})),
-  		transition('void => *', animate(0)),
+  		transition('void => *', animate(1000)),
   		transition('* => *', animate(500))
   	])
   ],
